08a84e194589a9d76d97410f46ace56e1cf19388
Egor Semenov <Hinodem@mail.ru>
2024-03-01 17:25:13 +0300

    Создана форма создания голосования

--- project/votes/urls.py
+++ project/votes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from votes.views import ListPolls
+from votes.views import PollCreateView
 app_name = 'votes'
 
 urlpatterns = [
-    path('list/', ListPolls.as_view(), name='list_polls'),
+    path('create/', PollCreateView.as_view(), name='create_polls'),
 #     path('create/', CreatePoll.as_view(), name='create_poll'),
 #     path('<uuid:vote_id>/', DetailPoll.as_view(), name='detail_poll'),
 #     path('<uuid:vote_id>/edit/', EditPoll.as_view(), name='edit_poll')
